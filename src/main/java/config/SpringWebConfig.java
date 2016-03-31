96ced50afdc0b7b850561921cd6812ed7833a3b4
simeon-nikolov <simeon.v.nikolov@gmail.com>
2016-03-31 21:44:05 +0300

    Added HomeController. Added some dependancies to pom.xml that are needed.

--- src/main/java/config/SpringWebConfig.java
+++ src/main/java/config/SpringWebConfig.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("dao")
+@ComponentScan({"dao", "controller", "config"})
 public class SpringWebConfig extends WebMvcConfigurerAdapter {
 	
     @Override
---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value={"/index", "/"})
+public class HomeController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String showIndex(Model model) {
+		return "index";
+	}
+}
