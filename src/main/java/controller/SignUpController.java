0b5003759442cead0788358d4a03f84e1b855c29
Yordan <jordancho@mail.bg>
2016-04-03 16:30:28 +0300

    SignUpController fixed

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -1,9 +1,8 @@
 package controller;
 
-
-
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,10 +10,26 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import config.BCrypt;
+import dao.IAccountDAO;
+import dao.IUserDAO;
+import exceptions.InvalidArgumentException;
+import model.Account;
+import model.Currency;
+import model.User;
 import view.model.UserViewModel;
 
 @Controller
 public class SignUpController {
+	
+	private final static String DEFAULT_STARTING_ACCOUNT_NAME = "Cash";
+	private final static Currency DEFAULT_CURRENCY = Currency.BGN;
+	private final static int DEFAULT_STARTING_ACCOUNT_BALANCE = 0;
+	
+	@Autowired
+	IUserDAO userDao;
+	@Autowired
+	IAccountDAO accountDao;
 
 	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
 	public String showSignUpPage(Model model) {
@@ -36,6 +51,32 @@ public class SignUpController {
 			model.addAttribute("errorMessage", "Both passwords must be the same!");
 			return "signUp";
 		}
+		
+		User userForDB = new User();
+		String hashedPassword = BCrypt.hashpw(userViewModel.getPassword(), BCrypt.gensalt());
+		try {
+			userForDB.setUsername(userViewModel.getUsername());
+			userForDB.setPassword(hashedPassword);
+			userForDB.setEmail(userViewModel.getEmail());
+			userForDB.setCurrency(DEFAULT_CURRENCY);
+			
+			int userID = userDao.addUser(userForDB);
+			userForDB.setId(userID);
+		} catch (InvalidArgumentException e) {			
+			e.printStackTrace();
+		}
+		Account defaultUserAccount = new Account();
+		
+		try {
+			defaultUserAccount.setUser(userForDB);
+			defaultUserAccount.setTitle(DEFAULT_STARTING_ACCOUNT_NAME);
+			defaultUserAccount.setBalance(DEFAULT_STARTING_ACCOUNT_BALANCE);
+			
+			accountDao.addAccount(defaultUserAccount);
+		} catch (InvalidArgumentException e) {			
+			e.printStackTrace();
+		}
+		
 		
 		return "login";
 	}
