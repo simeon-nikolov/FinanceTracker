08a4204619cc80ad28567d9726f2a6b7111c79f5
simeon-nikolov <simeon.v.nikolov@gmail.com>
2016-04-04 00:54:56 +0300

    Login now works with encrypted/encoded password in the database.

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -22,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder registry) throws Exception {
-		registry.userDetailsService(customUserDetailsService);
+		registry.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Override
@@ -32,28 +29,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/login", "/login**", "/signUp", "/logout").permitAll()
-				.antMatchers("/admin", "/admin/**").hasRole("ADMIN").anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").loginProcessingUrl("/login").failureUrl("/login/error").permitAll();
-	}
-
-	@Autowired
-    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService);
-        auth.authenticationProvider(authenticationProvider());
-    }
-     
+		http.csrf()
+			.disable().authorizeRequests()
+			.antMatchers("/login", "/login**", "/signUp", "/logout").permitAll()
+			.antMatchers("/admin", "/admin/**").hasRole("ADMIN")
+			.anyRequest().authenticated().and().formLogin()
+			.loginPage("/login")
+			.loginProcessingUrl("/login")
+			.failureUrl("/login").permitAll();
+	}     
      
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-     
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(customUserDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
     }
 }
