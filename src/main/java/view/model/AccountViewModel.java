fd2aa9aa9a7fa35b4034c436da0273a5737f0b65
Симеон Николов <simeon.v.nikolov@gmail.com>
2016-04-05 17:00:31 +0300

    Merge fix.

--- src/main/java/view/model/AccountViewModel.java
+++ src/main/java/view/model/AccountViewModel.java
@@ -6,17 +6,10 @@ import javax.validation.constraints.Size;
 import model.Currency;
 
 public class AccountViewModel {
-<<<<<<< HEAD
-	private int id;
-	
-=======
-	
 	private int id;
 
->>>>>>> 17d4ea8ee292ae277a98bc8ad384950b343c6809
 	@Size(min = 4, max = 15)
 	private String title;
-	
 	
 	private float balance;
 	
@@ -24,11 +17,7 @@ public class AccountViewModel {
 	
 	public AccountViewModel() {}	
 
-<<<<<<< HEAD
-	public AccountViewModel(int id, String title, int balance) {
-=======
 	public AccountViewModel(int id, String title, int balance, Currency currency) {
->>>>>>> 17d4ea8ee292ae277a98bc8ad384950b343c6809
 		this.id = id;
 		this.title = title;
 		this.balance = balance;
@@ -59,21 +48,11 @@ public class AccountViewModel {
 		this.balance = balance;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public Currency getCurrency() {
 		return currency;
 	}
 
 	public void setCurrency(Currency currency) {
 		this.currency = currency;
-	};
-	
-	
+	}
 }
